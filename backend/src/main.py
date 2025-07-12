29ede60c520b27c791d6072cb6f448066f779b6a
Saadat-Antor <md.sadat.amin@gmail.com>
2025-07-11 22:28:07 -0400

    .env created and API_KEY added in backend/src/main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
+
+API_KEY = os.environ.get("API_KEY")
+if not API_KEY:
+    raise NotImplementedError("'API_KEY' was not set")
 
 @app.get("/")
 def read_index():
